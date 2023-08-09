82856a51c38f4a5c5bca58037881994b9c68ca55
AnthoniaEfe <anthoniaefe36@gmail.com>
2023-08-09 23:59:21 +0100

    colors

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <div className="w-screen h-full ">
      
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,24 @@
 // tailwind.config.js
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        Vesta: {
+          100: "7239A1",
+          200: "#642598",
+          300: "#550F8E",
+          
+          700: "#36025F",
+          800: "#1F0738",
+          900: "#170A25",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};

f190a52ddf251fcd06aa95d284b18863d75aedab
AnthoniaEfe <anthoniaefe36@gmail.com>
2023-08-09 23:07:08 +0100

    config tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import "./App.css";
-import Coming from "./pages/coming";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Coming />
+     
     </div>
   );
 }

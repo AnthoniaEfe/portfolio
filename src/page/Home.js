8ad17634cf0a0b78af36a532468683a3ee56b5e2
AnthoniaEfe <anthoniaefe36@gmail.com>
2023-08-10 01:38:50 +0100

    links

--- src/page/Home.js
+++ src/page/Home.js
@@ -3,20 +3,38 @@ import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
-    <div className="relative bg-gradient-to-b from-vesta-10 via-vesta-9 to-vesta-8 w-scrren h-screen overflow-hidden p-16">
+    <div className="relative bg-gradient-to-b from-vesta-10 via-vesta-9 to-vesta-9 w-scrren h-screen overflow-hidden p-16">
       <Navbar className="fixed" />
-
-      <h1 className="text-white text-4xl font-bold">
-        Hi, I'm{" "}
-        <a
-          href="https://twitter.com/Anthonia_Efe"
-          target="_blank"
-          rel="noreferrer"
-          className="underline hover:scale-110 transform underline-offset-2"
+      <div
+        className="flex-col items-center justify-center
+      "
+      >
+        <h1
+          className="text-white text-4xl font-bold m-8
+      "
         >
-          Anthonia
-        </a>
-      </h1>
+          Hi, I'm{" "}
+          <a
+            href="https://twitter.com/Anthonia_Efe"
+            target="_blank"
+            rel="noreferrer"
+            className="underline hover:scale-120 underline-offset-2 "
+          >
+            Anthonia
+          </a>
+        </h1>
+
+        <p className="text-xl text-white">
+          A software developer, web designer and makeup artist
+        </p>
+
+        <div className="flex gap-16 text-white font-bold">
+          <a href="https://twitter.com/Anthonia_Efe">T</a>
+          <a href="https://www.linkedin.com/in/anthoniaefe/">L</a>
+          <a href="https://github.com/AnthoniaEfe">G</a>
+          <a href="mailto:anthoniaefe36@gmail.com">M</a>
+        </div>
+      </div>
     </div>
   );
 }

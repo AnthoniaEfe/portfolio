a18246b93610f8ddb776ed21c1198bb8ad6d5133
AnthoniaEfe <anthoniaefe36@gmail.com>
2023-08-13 01:51:15 +0100

    grid update

--- src/page/Home.js
+++ src/page/Home.js
@@ -10,8 +10,8 @@ export default function Home() {
   return (
     <div
       className="relative bg-gradient-to-b from-vesta-10 via-vesta-9 to-vesta-9
-     w-screen h-screen overflow-x-hidden text-white bg-stars bg-cover
-      bg-center bg-opacity-10 bg-blend-darken  backdrop-brightness-50
+     w-screen h-screen overflow-x-hidden text-white bg-stars bg-cover 
+      bg-scroll bg-opacity-10 bg-blend-darken 
      "
     >
       <Navbar className="sticky top-0 left-0" />{" "}
@@ -31,11 +31,11 @@ export default function Home() {
               rel="noreferrer"
               className=" transform hover:underline hover-md:text-5xl transition-transform underline-offset-2 "
             >
-              Anthonia Efe
+              Anthonia
             </a>
           </p>
 
-          <div className="w-5/6 h-full border-2 rounded-xl shadow-vesta-4 shadow-lg border-vesta-6 mx-auto my-8 p-6 md:p-8 justify-center items-center">
+          <div className="w-11/12 md:w-5/6 h-full border-2 rounded-xl shadow-vesta-4 shadow-lg border-vesta-6 mx-auto my-8 p-6 md:p-8 justify-center items-center">
             <p className="text-sm md:text-xl mb-4 text-justify">
               Nulla et ipsum velit voluptate occaecat culpa est sunt. Ipsum id
               adipisicing consectetur magna. Voluptate deserunt tempor ex duis
@@ -54,39 +54,28 @@ export default function Home() {
               excepteur laboris labore voluptate ipsum anim et. Nulla et ipsum
               velit voluptate occaecat culpa est sunt. Ipsum id adipisicing
               consectetur magna. Voluptate deserunt tempor ex duis cupidatat
-              magna nulla ullamco. Veniam sint nostrud nisi excepteur laboris
-              labore voluptate ipsum anim et. Nulla et ipsum velit voluptate
-              occaecat culpa est sunt. Ipsum id adipisicing consectetur magna.
-              Voluptate deserunt tempor ex duis cupidatat magna nulla ullamco.
-              Veniam sint nostrud nisi excepteur laboris labore voluptate ipsum
-              anim et. Nulla et ipsum velit voluptate occaecat culpa est sunt.
-              Ipsum id adipisicing consectetur magna. Voluptate deserunt tempor
-              ex duis cupidatat magna nulla ullamco. Veniam sint nostrud nisi
-              excepteur laboris labore voluptate ipsum anim et. Nulla et ipsum
-              velit voluptate occaecat culpa est sunt. Ipsum id adipisicing
-              consectetur magna. Voluptate deserunt tempor ex duis cupidatat
-              magna nulla ullamco. Veniam sint nostrud nisi excepteur laboris
-              labore voluptate ipsum anim et. Nulla et ipsum velit voluptate
-              occaecat culpa est sunt. Ipsum id adipisicing consectetur magna.
-              Voluptate deserunt tempor ex duis cupidatat magna nulla ullamco.
-              Veniam sint nostrud nisi excepteur laboris labore voluptate ipsum
-              anim et.
+              magna nulla ullamco.
             </p>
           </div>
         </div>
       </main>
-      <footer className=" bottom-0 left-0 p-4 bg-vesta-9">
-        <p className="mb-2 text-xl">Contact me:</p>
+      <footer
+        className=" bottom-0 left-0 right-0 p-4 bg-vesta-9 
+      flex flex-col justify-center items-center bg-opacity-70 rounded-t-lg"
+      >
+        <p className="mb-2 text-lg md:text-xl">Contact me:</p>
+        {/*  hidden, hide p in modbile view */}
         <div
-          className="grid grid-cols-4 w-screen md:gap-12 md:px-8
-           justify-center items-center gap-8 p-4
+          className="grid grid-cols-4 w-full md:gap-12
+           justify-center items-center gap-8 py-2 px-4 md:px-16
+           
            "
         >
           <a
             href="https://twitter.com/Anthonia_Efe"
             target="_blank"
             rel="noreferrer"
-            className="grid-child"
+            className="grid-child  "
           >
             <Twitter className="icon " />
           </a>
@@ -95,7 +84,7 @@ export default function Home() {
             href="https://www.linkedin.com/in/anthoniaefe/"
             target="_blank"
             rel="noreferrer"
-            className="grid-child "
+            className="grid-child  "
           >
             <Linkedin className="icon " />
           </a>
@@ -104,7 +93,7 @@ export default function Home() {
             href="https://github.com/AnthoniaEfe"
             target="_blank"
             rel="noreferrer"
-            className="grid-child"
+            className="grid-child "
           >
             <Github className="icon " />
           </a>

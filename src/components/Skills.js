1ef9e34e2eaa6d6dc8c6fdf82910a7adba2f8cfb
AnthoniaEfe <anthoniaefe36@gmail.com>
2023-08-21 23:43:53 +0100

    skill img

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,15 +3,45 @@ import React from "react";
 export default function Skills() {
   return (
     <div id="skills" className="skills">
-      <div className="skill">HTML\CSS</div>
-      <div className="skill">TailwindCSS</div>
-      <div className="skill">Javascript</div>
-      <div className="skill">ReactJS</div>
-      <div className="skill">TypeScript</div>
-      <div className="skill">NodeJS</div>
-      <div className="skill">Linux</div> <div className="skill">Git</div>
-      <div className="skill">Figma</div>
-      <div className="skill">C</div>
+      <div className="skill">
+        <img src="" alt="" />
+        HTML\CSS
+      </div>
+      <div className="skill">
+        <img src="" alt="" />
+        TailwindCSS
+      </div>
+      <div className="skill">
+        <img src="" alt="" />
+        Javascript
+      </div>
+      <div className="skill">
+        <img src="" alt="" />
+        ReactJS
+      </div>
+      <div className="skill">
+        <img src="" alt="" />
+        TypeScript
+      </div>
+      <div className="skill">
+        <img src="" alt="" />
+        NodeJS
+      </div>
+      <div className="skill">
+        <img src="" alt="" />
+        Linux
+      </div>
+      <div className="skill">
+        <img src="" alt="" />
+        Git
+      </div>
+      <div className="skill">
+        <img src="" alt="" />
+        Figma
+      </div>
+      <div className="skill">
+        <img src="" alt="" />C
+      </div>
     </div>
   );
 }
